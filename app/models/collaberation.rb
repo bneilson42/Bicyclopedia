d22bf44f342263814236289727ed098322b5f277
Brad Neilson <bneilson42@ymail.com>
2014-12-05 03:40:02 +0000

    Gnarly dude

--- app/models/collaberation.rb
+++ app/models/collaberation.rb
@@ -1,5 +1,5 @@
 class Collaboration < ActiveRecord::Base
-  attr_accessible :role, :wiki, :user, :wiki_id, :email
+  # attr_accessible :role, :wiki, :user, :wiki_id, :email
 
   belongs_to :wiki
   belongs_to :user
