aacac950f94c8df38e48c738d8f74c2325d3470a
Brad Neilson <bneilson42@ymail.com>
2014-12-16 15:33:16 -0800

    wiki

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -7,7 +7,7 @@ class WikisController < ApplicationController
 
   def show
     @wiki = Wiki.find(params[:id])
-    @possible_collaborators = User.subscribed.collect {|u| [u.name, u.id]}
+    @possible_collaborators = User.all.collect {|u| [u.name, u.id]}
     @collaboration = Collaboration.new
     if request.path != wiki_path(@wiki)
       redirect_to @wiki, status: :moved_permanently
